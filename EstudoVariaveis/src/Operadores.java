9f380be1e924c90768646fdc6c4e342fcf1b54f4
Italo Ferreira <itallo.fs@gmail.com>
2023-05-08 17:27:33 -0300

    dando sequencia aos estudos

---
+++ EstudoVariaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+       
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 || condicao2){
+            System.out.println("as duas condiçoes sao verdadeiras!");
+        }
+        else{
+            System.out.println("as duas condicoes sao falsas!");
+        }
+    }
+}
