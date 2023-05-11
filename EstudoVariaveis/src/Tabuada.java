8cccc86c69567184ed9412380494df1dd5de60d0
Italo Ferreira <itallo.fs@gmail.com>
2023-05-11 11:15:39 -0300

    Mais codigos, aprimorando e estudando.

---
+++ EstudoVariaveis/src/Tabuada.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Tabuada {
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Programa de Tabuada! ");
+
+        System.out.println("Informe a tabuada que deseja: ");
+        String tabuada = scanner.next();
+
+        int n1 = Integer.parseInt(tabuada);
+        int contador = 1;
+
+        while (contador <=10){
+            int multiplica = n1 * contador;
+            System.out.println(tabuada + " x " + contador + " = " + multiplica);
+            contador++;
+        }   
+    }
+}
